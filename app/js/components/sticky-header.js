bdcec3c7ebe2dd79f2a3a63342912ef24c27e265
Arsen Asanov <tatarin08082002@gmail.com>
2024-02-18 01:04:20 +0300

    added header and section-1

---
+++ app/js/components/sticky-header.js
@@ -0,0 +1,25 @@
+if (document.querySelector(".header")) {
+  // инициализируем top Navigation
+  const topNavigation = document.querySelector(".header");
+  // попап меню - блок
+  // const popupMenu = document.getElementById("popup-menu");
+  // Функция добавляет класс элементу в зависимости от координат окна
+  function checkСoordinatesElem(elem) {
+    // запуск функции по движению скролла
+    window.addEventListener("scroll", function () {
+      // инициализируем координаты окна по Y
+      const coordWindow = window.scrollY;
+      // если координаты окна больше 80, то добавляем класс, иначе - нет
+      coordWindow > 0
+        ? elem.classList.add("active")
+        : elem.classList.remove("active");
+      // coordWindow > 0
+      //   ? (elem.classList.add("active", "container"),
+      //     popupMenu.classList.add("top-scroll"))
+      //   : (elem.classList.remove("active", "container"),
+      //     popupMenu.classList.remove("top-scroll"));
+    });
+  }
+
+  checkСoordinatesElem(topNavigation);
+}
